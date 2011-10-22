a45135ac8cc1621fee5e533134769195caa8e8ad
Rich Warren <rikiwarren@Rich-Warrens-MacBook-Pro.local>
2011-10-21 23:36:20 -0500

    Forgot to update TabBarController's viewDidLoad

--- Health Beat/TabBarController.m
+++ Health Beat/TabBarController.m
@@ -43,31 +43,55 @@
 {
     [super viewDidLoad];
     
-    self.weightHistory = [[WeightHistory alloc] init];
-    
-    // Create a stack and load it with the view controllers from our tabs.
-    NSMutableArray* stack = 
-    [NSMutableArray arrayWithArray:self.viewControllers];
-    
-    // While we still have items on our stack
-    while ([stack count] > 0) {
-        
-        // pop the last item off the stack.
-        id controller = [stack lastObject];
-        [stack removeLastObject];
-        
-        // If it is a container object, add its controllers to the stack.
-        if ([controller respondsToSelector:@selector(viewControllers)]) {
-            
-            [stack addObjectsFromArray:[controller viewControllers]];
-        }
-        
-        // If it responds to setWeightHistory, set the weight history.
-        if ([controller respondsToSelector:@selector(setWeightHistory:)]) {
-            
-            [controller setWeightHistory:self.weightHistory];
-        }
-    }    
+    [WeightHistory accessWeightHistory:
+     ^(BOOL success, WeightHistory *weightHistory) {
+         
+         if (!success) {
+             
+             // An error occurred while instantiating our history.
+             // This probably indicates a catastrophic failure
+             // (e.g. the device's hard drive is out of space).
+             // We should really alert the user and tell them to 
+             // take appropriate action. For now, just throw 
+             // an exception.
+             
+             [NSException 
+              raise:NSInternalInconsistencyException
+              format:@"An error occured while trying to "
+              @"instantiate our history"];
+             
+         }
+         
+         self.weightHistory = weightHistory;
+         
+         // create a stack, and load it with the view 
+         // controllers from our tabs
+         NSMutableArray* stack = 
+         [NSMutableArray arrayWithArray:self.viewControllers];
+         
+         // while we still have items on our stack
+         while ([stack count] > 0) {
+             
+             // pop the last item off the stack
+             id controller = [stack lastObject];
+             [stack removeLastObject];
+             
+             // if it is a container object, add its view 
+             // controllers to the stack
+             if ([controller 
+                  respondsToSelector:@selector(viewControllers)]) {
+                 
+                 [stack addObjectsFromArray:[controller viewControllers]];
+             }
+             
+             // if it responds to setWeightHistory, set the weight history
+             if ([controller 
+                  respondsToSelector:@selector(setWeightHistory:)]) {
+                 
+                 [controller setWeightHistory:self.weightHistory];
+             }
+         }   
+     }];
 }
 
 - (void)viewDidUnload
